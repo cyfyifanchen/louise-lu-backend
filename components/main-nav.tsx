b89ae7e7b4431dccda8610dd87ada0c3fb53c202
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-09-16 09:35:31 +0800

    Adding billboards, routes, and billboard-forms

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -13,7 +13,17 @@ export function MainNav({
 
   const routes = [
     {
-      href: `/${params.storeId}`,
+      href: `/${params.storeId}/`,
+      label: "Home",
+      active: pathname === `/${params.storeId}/`,
+    },
+    {
+      href: `/${params.storeId}/billboards`,
+      label: "Billboards",
+      active: pathname === `/${params.storeId}/billboard`,
+    },
+    {
+      href: `/${params.storeId}/settings`,
       label: "Settings",
       active: pathname === `/${params.storeId}/settings`,
     },
