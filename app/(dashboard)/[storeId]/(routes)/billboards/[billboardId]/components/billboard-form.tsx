01b896bbc94720faf729654a11192e1dbbb05f45
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-09-17 09:21:07 +0800

    Something is wrong

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -1,14 +1,17 @@
 "use client"
 
 import * as z from "zod"
-import { Billboard, Store } from "@prisma/client"
-import { Heading } from "@/components/ui/heading"
-import { Button } from "@/components/ui/button"
-import { Trash } from "lucide-react"
-import { Separator } from "@/components/ui/separator"
-import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
+import axios from "axios"
 import { useState } from "react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { toast } from "react-hot-toast"
+import { CloudFog, Trash } from "lucide-react"
+import { Billboard } from "@prisma/client"
+import { useParams, useRouter } from "next/navigation"
+
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 import {
   Form,
   FormControl,
@@ -17,39 +20,34 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import toast from "react-hot-toast"
-import axios from "axios"
-import { useParams, useRouter } from "next/navigation"
+import { Separator } from "@/components/ui/separator"
+import { Heading } from "@/components/ui/heading"
 import { AlertModal } from "@/components/modals/alert-modal"
-import { ApiAlert } from "@/components/ui/api-alert"
-import { useOrigin } from "@/hooks/use-origin"
 import ImageUpload from "@/components/ui/image-upload"
 
-interface BillboardFormProps {
-  initialData: Billboard | null
-}
-
 const formSchema = z.object({
-  label: z.string().min(3, "Too short"),
+  label: z.string().min(1),
   imageUrl: z.string().min(1),
 })
 
 type BillboardFormValues = z.infer<typeof formSchema>
+
+interface BillboardFormProps {
+  initialData: Billboard | null
+}
 
 export const BillboardForm: React.FC<BillboardFormProps> = ({
   initialData,
 }) => {
   const params = useParams()
   const router = useRouter()
-  const origin = useOrigin()
 
   const [open, setOpen] = useState(false)
   const [loading, setLoading] = useState(false)
 
   const title = initialData ? "Edit billboard" : "Create billboard"
-  const description = initialData ? "Edit a billboard" : "Add a new billboard"
-  const toastMessage = initialData ? "Billboard updated" : "Billboard created"
+  const description = initialData ? "Edit a billboard." : "Add a new billboard"
+  const toastMessage = initialData ? "Billboard updated." : "Billboard created."
   const action = initialData ? "Save changes" : "Create"
 
   const form = useForm<BillboardFormValues>({
@@ -63,11 +61,19 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
   const onSubmit = async (data: BillboardFormValues) => {
     try {
       setLoading(true)
-      await axios.patch(`/api/stores/${params.storeId}`, data)
+      if (initialData) {
+        await axios.patch(
+          `/api/${params.storeId}/billboards/${params.billboardId}`,
+          data
+        )
+      } else {
+        await axios.post(`/api/${params.storeId}/billboards`, data)
+      }
       router.refresh()
-      toast.success("Store updated.")
-    } catch (error) {
-      toast.error("Something went wrong.")
+      router.push(`/${params.storeId}/billboards`)
+      toast.success(toastMessage)
+    } catch (error: any) {
+      toast.error("you son of a bitch.")
     } finally {
       setLoading(false)
     }
@@ -76,13 +82,18 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
   const onDelete = async () => {
     try {
       setLoading(true)
-      await axios.delete(`/api/stores/${params.storeId}`)
+      await axios.delete(
+        `/api/${params.storeId}/billboards/${params.billboardId}`
+      )
       router.refresh()
-      router.push("/")
-      toast.success("Store deleted.")
-    } catch (error) {
-      toast.error("Make you removed all the products and categories first.")
+      router.push(`/${params.storeId}/billboards`)
+      toast.success("Billboard deleted.")
+    } catch (error: any) {
+      toast.error(
+        "Make sure you removed all categories using this billboard first."
+      )
     } finally {
+      setLoading(false)
       setOpen(false)
     }
   }
@@ -101,7 +112,7 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
           <Button
             disabled={loading}
             variant="destructive"
-            size="icon"
+            size="sm"
             onClick={() => setOpen(true)}
           >
             <Trash className="h-4 w-4" />
@@ -132,7 +143,7 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
               </FormItem>
             )}
           />
-          <div className="grid grid-cols-3 gap-8">
+          <div className="md:grid md:grid-cols-3 gap-8">
             <FormField
               control={form.control}
               name="label"
@@ -156,7 +167,6 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
           </Button>
         </form>
       </Form>
-      <Separator />
     </>
   )
 }
